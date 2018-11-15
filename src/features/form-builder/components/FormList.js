58f1e665b84837869adf3e774dc01be36d4a0bf1
Michael <michaelkallies88@gmail.com>
2018-11-15 12:43:34 -0500

    Show empty state for preview

--- src/features/form-builder/components/FormList.js
+++ src/features/form-builder/components/FormList.js
@@ -2,24 +2,29 @@ import React from 'react'
 import { List, Header } from 'semantic-ui-react'
 
 function FormList({ forms, selectForm }) {
+  console.log('forms', forms)
   return (
     <div className="preview__form-list">
       <Header content="Forms" />
-      <List
-        celled
-        selection
-        animated
-        size="big"
-        className="preview__list-container"
-      >
-        {forms.map((form, idx) => {
-          return (
-            <List.Item key={idx} onClick={() => selectForm(form)}>
-              {form.title}
-            </List.Item>
-          )
-        })}
-      </List>
+      {forms.length ? (
+        <List
+          celled
+          selection
+          animated
+          size="big"
+          className="preview__list-container"
+        >
+          {forms.map((form, idx) => {
+            return (
+              <List.Item key={idx} onClick={() => selectForm(form)}>
+                {form.title}
+              </List.Item>
+            )
+          })}
+        </List>
+      ) : (
+        <div>No forms to display</div>
+      )}
     </div>
   )
 }
